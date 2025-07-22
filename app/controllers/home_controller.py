b7d971cc48a15bfa05533113c23a990127e3471b
Tianyi Yang <tyang21@stevens.edu>
2025-07-21 21:14:17 -0400

    CHange slide pages into for loop

--- app/controllers/home_controller.py
+++ app/controllers/home_controller.py
@@ -6,4 +6,13 @@ home_bp = Blueprint('home', __name__)
 @home_bp.route('/')
 def render_home():
     featured_items = Product.query.limit(3).all()  # top 3 products
-    return render_template('index.html', featured_items=featured_items)
+
+    carousel_images = [
+        'images/slide1.jpg',
+        'images/testimg2.jpg',
+        'images/slide3.jpg',
+    ]
+
+    return render_template('index.html',
+                           featured_items=featured_items,
+                           carousel_images=carousel_images)
