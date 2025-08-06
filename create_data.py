de7b7dd5b2f1200d4091bc99612aaf12f31dd9d3
Nanofication <nansquared@gmail.com>
2025-08-05 21:23:38 -0400

    Updated db with real data

--- create_data.py
+++ create_data.py
@@ -2,43 +2,41 @@ from run import app
 from app import db
 from app.models.product import Product
 
-'''
 with app.app_context():
     db.drop_all()
     db.create_all()
 
     # Add fresh data
-    p1 = Product(name="Bone Toy", category="dog", image_url="images/testimg1.jpg")
-    p2 = Product(name="Catnip Mouse", category="cat", image_url="images/testimg3.jpg")
-    p3 = Product(name="Whatever", category="dog", image_url="images/testimg2.jpg")
+    p1 = Product(name="Dog Poop Bag", category="dog", image_url="images/dog_poop_bag_1.jpeg")
+    p2 = Product(name="Dog Poop Bag Multicolor", category="dog", image_url="images/dog_poop_bag_4.jpeg")
+    p3 = Product(name="Dog Poop Bag Dispenser", category="dog", image_url="images/dog_poop_bag_dispenser_1.jpeg")
 
     db.session.add_all([p1, p2, p3])
     db.session.commit()
 
     print("Database reset and seeded.")
-    
-'''
 
-with app.app_context():
 
-    #add amount for "top items" filter 
-    products = Product.query.all()
-    for p in products:
-        if p.amount is None:
-            p.amount = 100
-    db.session.commit()
-    print("Existing products updated with amount.")
-    
-    #add more data
-    example = Product(
-        name="Example3",
-        category="dog",
-        image_url="images/dog1.jpg"
-    )
-
-    db.session.add(example)
-    db.session.commit()
-    print("New product 'example' added.")
+# with app.app_context():
+#
+#     #add amount for "top items" filter
+#     products = Product.query.all()
+#     for p in products:
+#         if p.amount is None:
+#             p.amount = 100
+#     db.session.commit()
+#     print("Existing products updated with amount.")
+#
+#     #add more data
+#     example = Product(
+#         name="Example3",
+#         category="dog",
+#         image_url="images/dog1.jpg"
+#     )
+#
+#     db.session.add(example)
+#     db.session.commit()
+#     print("New product 'example' added.")
     '''
     #delete data
     last_product = Product.query.order_by(Product.id.desc()).first()
